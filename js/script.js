8329c9158d2c488e473477da75cabd69de882f74
honokame <honoka.happy.0613@icloud.com>
2024-06-24 23:46:58 +0900

    add error check

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-document.getElementById('convert').addEventListener('click', convertToBinary);
+document.getElementById("convert").addEventListener("click", convertToBinary);
 
-function convertToBinary(){
-  const inputDecimal = document.getElementById('inputDecimal').value;
-	const outputBinary = document.getElementById('outputBinary');
-  
-	const decimalNumber = parseInt(inputDecimal, 10);
-	const result = decimalNumber.toString(2);
-  outputBinary.textContent = result;
+function convertToBinary() {
+  const inputDecimal = document.getElementById("inputDecimal").value;
+  const outputBinary = document.getElementById("outputBinary");
+
+  const decimalNumber = parseInt(inputDecimal, 10);
+  if (isNaN(decimalNumber)) {
+    outputBinary.textContent = "無効な入力です";
+  } else {
+    const result = decimalNumber.toString(2);
+    outputBinary.textContent = result;
+  }
 }
